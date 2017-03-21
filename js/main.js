602490d9aef87db88af926a644ed6fd645e9ce6c
Katsiaryna_Shastakov <Katsiaryna_Shastakova@epam.com>
2017-03-21 14:57:04 +0300

    add circles

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let canvas = document.getElementById('snake-game');
+let step = 60;
 
 if (canvas.getContext) {
  var ctx = canvas.getContext('2d');
@@ -20,10 +21,28 @@ for ( var i = 60; i < canvas.getAttribute("height"); i+=60 ) {
   ctx.stroke();
 }
 
-let img = new Image();
-img.addEventListener('load', function() {
+let circle = new Path2D();
+  circle.arc(330, 330, 20, 0, 2 * Math.PI); //сдвиг круга относительно координат = 30px
+  ctx.fillStyle = "#8be400";
+  ctx.fill(circle);
+
+let circle2 = new Path2D();
+  circle2.arc(30, 30, 20, 0, 2 * Math.PI);
+  ctx.fillStyle = "#fff404";
+  ctx.fill(circle2);
+
+let circle3 = new Path2D();
+  circle3.arc(270, 150, 20, 0, 2 * Math.PI);
+  ctx.fillStyle = "#ff2c10";
+  ctx.fill(circle3);
+
+//@TODO random coordinates
+//@TODO random colors of circles
+
+let snakeHead = new Image();
+snakeHead.addEventListener('load', function() {
   console.log('Image was loaded');
-  ctx.drawImage(img, 300, 540);
+  ctx.drawImage(snakeHead, 300, 540);
 })
 
-img.src = 'img/snake-head.png';
+snakeHead.src = 'img/snake-head.png';
